4b674785372e2a6744431ff11f1726c8834515a9
JUNGSUKWON <jsw782@naver.com>
2021-12-27 11:43:31 +0900

    start menu

---
+++ src/main/java/com/aiproject/menu/web/HelloController.java
@@ -0,0 +1,27 @@
+package com.aiproject.menu.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+    @PostMapping("/api/v1/senddata")
+    public String sendPostdata(@RequestParam(value="data",defaultValue = "없음")String data){
+        
+        return "당신이 입력한 데이터는 ?"+data;
+
+    }
+
+    @GetMapping("/api/v1/sendgetdata")
+    public String sendGetdata(@RequestParam(value="data",defaultValue = "없음")String data){
+        
+        return "당신이 입력한 데이터는 ?"+data;
+
+    }
+
+
+
+}
